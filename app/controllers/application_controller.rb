cfe9a69f8c5b15b52139d0ac8fccfb24037060ee
Martin Carel <martin.carel@gmail.com>
2012-10-17 21:40:49 -0400

    refactoring + dealing with root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :deal_with_root
+
   protect_from_forgery
 
   def welcome
   end
 
   def browse
-    params[:level1] = nil if params[:level1] == 'root'
     path = params[:level1]
     path += "/#{params[:level2]}" if params[:level2]
     path += "/#{params[:level3]}" if params[:level3]
@@ -17,14 +18,30 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
   def file_list dir
-    dir = "" if dir.nil?
-    path = "app/views/notes/" + dir
+    path = "notes/" + (dir || "")
     if File.directory? path
       entries = Dir.entries(path)
-      entries.reject{|f| f =~ /^\./}
+      entries.reject!{|f| f =~ /^\./}
+      filter_markdown_files(path, entries)
     else
       [ path ]
     end
   end
+
+  def filter_markdown_files path, entries
+    entries.select do |e|
+      if File.directory?(path + "/" + e)
+        true
+      else
+        e =~ /.*\.md$/
+      end
+    end
+  end
+
+  def deal_with_root
+    params[:level1] = nil if params[:level1] == 'root'
+  end
+
 end
