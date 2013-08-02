0791530629d3875b78d9f630a61234489e99e6fa
Martin Carel <martin.carel@gmail.com>
2013-08-01 23:57:26 -0400

    images: inserts base path for images embedded in notes

--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -1,13 +1,27 @@
 require 'rdiscount'
 
 module MarkdownHandler
+
   def self.erb
     @erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
   def self.call(template)
     compiled_source = erb.call(template)
-    "RDiscount.new(begin;#{compiled_source};end).to_html"
+
+    # trick to have the correct url for images in markdown source
+    # assuming the image is in the same location as the notes file
+    begin
+      path = template.inspect
+      path.gsub!('public', '')
+      # take everything up to the last slash
+      image_path_base = path.match(/.*\//)[0]
+      compiled_source.gsub!("](", "](" + image_path_base)
+    rescue
+      # fail silently since this is a fragile hack
+    end
+
+    output = "RDiscount.new(begin;#{compiled_source};end).to_html"
   end
 end
 
