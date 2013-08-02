5267887a35825c15c8b28861176ff53d7afb5904
Martin Carel <martin.carel@gmail.com>
2013-08-01 20:30:28 -0400

    adds a working breadcrumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
     path += "/#{file}"
   end
 
+  def breadcrumb_link memo, segment
+    path = if memo.blank?
+             segment
+           else
+             memo + '/' + segment
+           end
+    "/browser" + "/#{path}"
+  end
+
 end
