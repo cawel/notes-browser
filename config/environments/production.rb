82a9ecbe83bdcb467cbe66d49373153bd830b469
Martin Carel <martin.carel@gmail.com>
2017-12-21 15:57:18 -0500

    config: allow rails to serve assets from the public folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,8 +10,8 @@ NotesViewer::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Enable/Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
