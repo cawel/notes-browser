ff99b178664a0be06be3d8127bb726d300a4373c
Martin Carel <martin.carel@gmail.com>
2018-01-10 15:00:28 -0500

    config: prepare heroku for rails 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,19 @@ NotesViewer::Application.configure do
   # Enable/Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_files = true
 
+  # Config required by heroku when using rails 5
+  # See https://github.com/heroku/rails_12factor
+
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
