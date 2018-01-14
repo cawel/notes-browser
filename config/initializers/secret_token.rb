522cedb5924a7115d8880bfd4af017244b32f293
Martin Carel <martin.carel@gmail.com>
2018-01-13 20:04:03 -0500

    remove old secret token file

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NotesViewer::Application.config.secret_token = '26d0f543d7100e9cef6d5139816564234783e70d89b3d203221fd45fe451c57e168858b62454ce2457323062da5cce1ed312f63b7d96d6e52af053cfd18f552f'
